bf26eb4d6e94d61aa9924d7d3e35fcf23749aed9
MinexAutomation <minex.automation@gmail.com>
2020-01-20 12:49:51 -0800

    Added an Environment.SpecialFolder.UserProfile-based implementation of the IUserProfileDirectoryPathProvider.

---
+++ source/R5T.Visigothia.Local.Default/Code/Services/Implementations/EnvironmentBasedUserProfileDirectoryPathProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace R5T.Visigothia.Local.Default
+{
+    /// <summary>
+    /// Uses the <see cref="Environment.GetFolderPath(Environment.SpecialFolder)"/> with <see cref="Environment.SpecialFolder.UserProfile"/> parameter to get the user-profile directory path.
+    /// </summary>
+    public class EnvironmentBasedUserProfileDirectoryPathProvider : IUserProfileDirectoryPathProvider
+    {
+        public string GetUserProfileDirectoryPath()
+        {
+            var userProfileDirectoryPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            return userProfileDirectoryPath;
+        }
+    }
+}
