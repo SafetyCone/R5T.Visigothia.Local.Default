60c39e04f98973c8fe00294cf10808e2cd16473f
MinexAutomation <minex.automation@gmail.com>
2020-01-20 12:46:19 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Visigothia.Local.Default.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Visigothia.Local.Default.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
