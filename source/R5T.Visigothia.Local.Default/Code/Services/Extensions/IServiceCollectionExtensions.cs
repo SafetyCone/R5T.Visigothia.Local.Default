aa3bba1faab5fa9500a526ca207eef802d4b6396
MinexAutomation <minex.automation@gmail.com>
2020-01-30 14:03:43 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

---
+++ source/R5T.Visigothia.Local.Default/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Visigothia.Local.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="EnvironmentBasedUserProfileDirectoryPathProvider"/> implementation of <see cref="IUserProfileDirectoryPathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddEnvironmentBasedUserProfileDirectoryPathProvider(this IServiceCollection services)
+        {
+            services.AddSingleton<IUserProfileDirectoryPathProvider, EnvironmentBasedUserProfileDirectoryPathProvider>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="EnvironmentBasedUserProfileDirectoryPathProvider"/> implementation of <see cref="IUserProfileDirectoryPathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IUserProfileDirectoryPathProvider> AddEnvironmentBasedUserProfileDirectoryPathProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IUserProfileDirectoryPathProvider>(() => services.AddEnvironmentBasedUserProfileDirectoryPathProvider());
+            return serviceAction;
+        }
+    }
+}
