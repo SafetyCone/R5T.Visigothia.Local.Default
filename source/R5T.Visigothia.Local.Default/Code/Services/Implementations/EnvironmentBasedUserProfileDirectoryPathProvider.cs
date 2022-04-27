2197be15ce4a9c44d56ec8d43e92323f1cde6a86
davidcoats <david@getsafetycone.com>
2022-04-27 15:37:40 -0700

    Interim changes.

--- source/R5T.Visigothia.Local.Default/Code/Services/Implementations/EnvironmentBasedUserProfileDirectoryPathProvider.cs
+++ source/R5T.Visigothia.Local.Default/Code/Services/Implementations/EnvironmentBasedUserProfileDirectoryPathProvider.cs
@@ -1,12 +1,12 @@
-﻿using System;
+using System;using R5T.T0064;
 
 
 namespace R5T.Visigothia.Local.Default
-{
+{[ServiceImplementationMarker]
     /// <summary>
     /// Uses the <see cref="Environment.GetFolderPath(Environment.SpecialFolder)"/> with <see cref="Environment.SpecialFolder.UserProfile"/> parameter to get the user-profile directory path.
     /// </summary>
-    public class EnvironmentBasedUserProfileDirectoryPathProvider : IUserProfileDirectoryPathProvider
+    public class EnvironmentBasedUserProfileDirectoryPathProvider : IUserProfileDirectoryPathProvider,IServiceImplementation
     {
         public string GetUserProfileDirectoryPath()
         {
